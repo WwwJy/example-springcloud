43a30d36398faf8519b31eebfb0892164ecb80f1
w景洋 <137127854@qq.com>
2019-07-08 17:21:17 +0800

    修改注册链接

--- example-admin/src/main/java/com/example/info/ExampleAdminApplication.java
+++ example-admin/src/main/java/com/example/info/ExampleAdminApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
